64ecccb7bf868f203b77844be8b355ba1251af94
jonnyweb <jon@jonhargest.co.uk>
2020-11-15 18:24:33 -0800

    improve hydration?

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react'
 import { hydrate } from 'react-dom'
 import { renderToString } from 'react-dom/server'
 import App from './App'
+import once from 'lodash.once'
 
 const app = document.getElementById('app')
 
-if (app && app.hasChildNodes()) {
-  hydrate(<App />, app)
-}
+const hydrateApp = once(() => {
+  if (app && app.hasChildNodes()) {
+    hydrate(<App />, app)
+  }
+})
+
+document.addEventListener('scroll', hydrateApp)
+document.addEventListener('mousemove', hydrateApp)
+document.addEventListener('click', hydrateApp)
 
 export default () => {
   return `<div id="app">${renderToString(<App />)}</div>`
