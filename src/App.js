d256f60e6a1da34af6c55d527aa6c5d6e170f61b
jonnyweb <jon@jonhargest.co.uk>
2022-11-19 15:19:27 -0600

    fix: bootstrap styling

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Contact from './Contact'
 import News from './News'
 
 import './global.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default class App extends Component {
   componentDidMount() {
